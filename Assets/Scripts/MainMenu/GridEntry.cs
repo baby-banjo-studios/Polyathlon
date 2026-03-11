82e79ddcc2cc5513b57af4a6b658063e5db719ec
Matt Graber <grabermtw@gmail.com>
2026-03-10 19:31:13 -0700

    Updated Mall images, fixed build problem

--- Assets/Scripts/MainMenu/GridEntry.cs
+++ Assets/Scripts/MainMenu/GridEntry.cs
@@ -71,7 +71,7 @@ public class GridEntry : Selectable, IPointerEnterHandler
 
     public new void Reset()
     {
-        base.Reset();
+        //base.Reset();
         currentSelectors.Clear();
         Redraw(true);
     }

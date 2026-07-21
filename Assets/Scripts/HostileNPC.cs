889b650ea1d6136dad799069ef32a831cd530f86
Matthew Vorsteg <44066626+mvorsteg@users.noreply.github.com>
2026-07-21 00:44:39 -0400

    Added sensor

--- Assets/Scripts/HostileNPC.cs
+++ Assets/Scripts/HostileNPC.cs
@@ -7,7 +7,24 @@ public class HostileNPC : Entity
 {    
 
     protected Racer target = null;
+    [SerializeField]
+    protected float engageDistance = 25f;
+    protected Sensor sensor;
+    public Entity target2;
 
-    
+    protected override void Awake()
+    {
+        base.Awake();
+        sensor = GetComponentInChildren<Sensor>();
+    }
 
+    protected override void Update()
+    {
+        base.Update();
+        if (sensor != null)
+        {
+            Entity target = sensor.GetClosestVisibleTarget();
+            target2 = target;
+        }
+    }
 }
---
+++ Assets/Scripts/Sensor.cs
@@ -0,0 +1,103 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    private class TargetInfo
+    {
+        public TargetInfo(Entity target, float distance, bool visible)
+        {
+            this.target = target;
+            this.distance = distance;
+            this.visible = visible;
+        }
+        public Entity target;
+        public float distance;
+        public bool visible;
+    }
+
+    private SphereCollider triggerCollider;
+    public float range;
+    public LayerMask blockingMask;
+    private List<TargetInfo> targetsInRange;
+
+    private void Awake()
+    {
+        targetsInRange = new List<TargetInfo>();
+
+        triggerCollider = gameObject.AddComponent<SphereCollider>();
+        triggerCollider.radius = range;
+        triggerCollider.isTrigger = true;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public Entity GetClosestVisibleTarget()
+    {
+        Entity currTarget = null;
+        float shortestDistance = Mathf.Infinity;
+        foreach (TargetInfo targetInfo in targetsInRange)
+        {
+            if (targetInfo.visible && targetInfo.distance < shortestDistance)
+            {
+                currTarget = targetInfo.target;
+                shortestDistance = targetInfo.distance;
+            }
+        }
+        return currTarget;
+    }
+
+    private void Update()
+    {
+        //
+        foreach (TargetInfo targetInfo in targetsInRange)
+        {
+            targetInfo.visible = IsTargetVisible(targetInfo.target);
+            targetInfo.distance = Vector3.Distance(transform.position, targetInfo.target.hips.position);
+            Debug.DrawLine(transform.position, targetInfo.target.hips.position, targetInfo.visible ? Color.green : Color.red);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Entity target))
+        {
+            TargetInfo newTarget = new TargetInfo(target, Vector3.Distance(transform.position, target.hips.position), IsTargetVisible(target));
+            targetsInRange.Add(newTarget);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out Entity target))
+        {
+            foreach (TargetInfo existingTarget in targetsInRange)
+            {
+                if (existingTarget.target == target)
+                {
+                    targetsInRange.Remove(existingTarget);
+                    break;
+                }
+            }
+        }
+    }
+
+    private bool IsTargetVisible(Entity target)
+    {
+        if (Physics.Linecast(transform.position, target.hips.position, out RaycastHit hit, blockingMask.value))
+        {
+            if (hit.collider.gameObject == target.gameObject)
+            {
+                // no obstacle between source and target
+                return true;
+            }
+            // hit something else
+            return false;
+        }
+        // no collision - can happen if we dont count the target's layermask
+        return true;
+    }
+}
